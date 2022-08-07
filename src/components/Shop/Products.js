f274bdbf25c5b66881b4fd00ca2c014c315fc86b
Ryan Cabanas <ryangithub777@gmail.com>
2022-08-07 18:51:26 -0400

    Lesson 252: Add a couple additional products to the list.

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -7,9 +7,19 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title="Test"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is a first product - amazing!"
+        />
+        <ProductItem
+          title="Chinese Junk"
+          price={0.5}
+          description="This product will break within a month"
+        />
+        <ProductItem
+          title="German Made"
+          price={20}
+          description="This product will last a lifetime"
         />
       </ul>
     </section>
