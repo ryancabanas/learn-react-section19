c6d445966a0b86039ac4828638a0aa91c750c7cc
Ryan Cabanas <ryangithub777@gmail.com>
2022-08-08 16:29:49 -0400

    End of Lesson 259: Sending data to the backend asynchronously using `useEffect` and Redux Toolkit.
    - Add a instructor-provided `Notification` component for the message notification
      bar at the top of the page, as well as a CSS styling module.
    
    - Update the `uiSlice` file with a `notification` state and a couple reducers
      to modify it.
    
    - Import a whole bunch of things into the `App` component to utilize the `useEffect`
      hook to fire off dispatch calls to the backend (via the notification reducer).
      `useEffect` will file whenever the cart store (which is listed as a `useEffect`
      dependency) changes.
    
      Since `useEffect` will also still fire the first time the page loads, but we
      don't want it to because we don't want to send empty cart data to the backend,
      we can add this `isInitialPageLoad` variable as a trick to exit the
      `useEffect` early before sending data to the backend on initial page load.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/uiSlice';
+import Notification from './components/UI/Notification';
+
+let isInitialPageLoad = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = await fetch(
+        'https://react-http-4bc80-default-rtdb.firebaseio.com/redux-store-cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed!');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Cart data sent successfully!',
+        })
+      );
+    };
+
+    if (isInitialPageLoad) {
+      isInitialPageLoad = false;
+      return;
+    }
+
+    sendCartData()
+      .then((empty) => {
+        setTimeout(() => {
+          dispatch(uiActions.hideNotification());
+        }, 1000);
+      })
+      .catch((error) => {
+        dispatch(
+          uiActions.showNotification({
+            status: 'error',
+            title: 'Error!',
+            message: error.message,
+          })
+        );
+      });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialUIState = {
   cartIsVisible: false,
+  notification: null,
 };
 
 const uiSlice = createSlice({
@@ -10,6 +11,16 @@ const uiSlice = createSlice({
   reducers: {
     toggleCartVisibility: (state) => {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification: (state, action) => {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+    hideNotification: (state) => {
+      state.notification = null;
     },
   },
 });
