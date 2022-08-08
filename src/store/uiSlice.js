f471d8ab977f01cad32831ae85b65f4f427da9a5
Ryan Cabanas <ryangithub777@gmail.com>
2022-08-07 21:50:40 -0400

    Lessons 252: Implement first slice named "UI".
    - Set up the store's `index.js` file.
    
    - Create a `uiSlice` module for managing the visibility of the cart and add it
      to the store module.
    
    - Import the `Provider` component from `react-redux` into the starting `index.js`
      file, import our store there too, and then wrap the main `App` component there
      with the `Provider` component to make our Redux store available everywhere.
    
    - Import our UI Slice actions and the `useDispatch` function from `react-redux`
      into the `CartButton` component and bind the button to a handler function to
      dispatch our "toggle cart visibility" reducer function.
    
    - Finally, don't forget to import our store (using `useSelector` from `react-redux`)
      to the `App` component so that we can use the state the decide whether, or not
      to show the cart.

---
+++ src/store/uiSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialUIState = {
+  cartIsVisible: false,
+};
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: initialUIState,
+  reducers: {
+    toggleCartVisibility: (state) => {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+export default uiSlice.reducer;
